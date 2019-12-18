ba5b0e0763bb7317c437fb4a54a42ae217dfdbd3
Matti <test@test.test>
2019-12-18 10:26:09 +0100

    Spielwörter änderungen

--- PROJECT/server/Server/Spielwörter.java
+++ PROJECT/server/Server/Spielwörter.java
@@ -12,53 +12,41 @@ import java.io.IOException;
 public class Spielwörter
 {
     String dateiname;
-    String[] BOI = new String[117];
-    
+    String[] wörter;
     
     public String gibNeueswort()
     {
-        
-        int i=1;
-        int x=1;
-        while(i!=0)
+        if (leseDateiAus())
         {
-            Random rand = new Random();
-            x = rand.nextInt(116);
-            if(BOI [x]!=""){i=0;}
+            return wörter[(int)(Math.random() * (wörter.length - 1))];
         }
-        String gewähltesWort = BOI [x];
-        BOI [x] = "";
-        return gewähltesWort;
+        else
+        {
+            return null;
+        }
     }
-    
-    public void leseDateiAus()
+
+    public boolean leseDateiAus()
     {
-        
-        File Wörterdatei = new File("T:/Klasse q2/BrandIF/Netzmalen Max/PROJECT/server/Server/Woerter.txt");
-        if (!Wörterdatei.canRead() || !Wörterdatei.isFile())
-            {System.out.println("Dateifehler");
-                System.exit(0);}
-        BufferedReader in = null;
-        try {
-            //in = new BufferedReader(new FileReader("T:\\Klasse q2\\BrandIF\\Netzmalen Max\\PROJECT\\server\\Server"));
-            in = new BufferedReader(new FileReader(Wörterdatei));
-            String zeile = null;
-            int i=116;
-            while ((zeile = in.readLine()) != null) {
-                BOI [i] = zeile;
-                i--;
+        File Wörterdatei = new File("T:/Klasse q2/BrandIF/Netzmalen Max/PROJECT/server/Server/Woerter.txt"); //TODO: Datei in Cloud
+        BufferedReader reader = null;
+        try
+        {
+            reader = new BufferedReader(new FileReader(Wörterdatei));
+            String inhalt = "";
+            while (reader.readLine() != null)
+            {
+                inhalt=inhalt+";"+reader.readLine();
             }
-        } catch (IOException e) {
+            wörter=inhalt.split(";");
+            return true;
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
-        } finally {
-            if (in != null)
-                try {
-                    in.close();
-                } catch (IOException e) {
-                }
-        } 
-        
+            Logger.error("File doesn't exists");
+            return false;
+        }
     }
 }
-
 
